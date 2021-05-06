62777816c7fbe2448fab8c855df8b9260db5994f
Piero Marini <piero.marini21@gmail.com>
2021-05-06 18:57:10 -0500

    remove gmail. cleanup rofi configs and changed theme

--- .config/rofi/menu/horarios.sh
+++ .config/rofi/menu/horarios.sh
@@ -1,1 +1,1 @@
-rofi -modi "Clases":"~/.config/rofi/rofi-json.sh ~/Development/horarios/horario.json" -show "Clases" -theme $HOME/.config/rofi/file-browser -theme-str 'configuration{show-icons: false;}'
+rofi -modi "Clases":"~/.config/rofi/rofi-json.sh ~/Development/horarios/horario.json" -show "Clases" -theme $HOME/.config/rofi/file-browser
